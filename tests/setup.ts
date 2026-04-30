8edc3b597fe8070e26d7baf67ac9b64279dd0b4e
juliendargelos <hello@julien.gl>
2026-04-30 21:33:11 +0200

    chore: suppress ffmpeg logs in tests with Log.setLevel(AV_LOG_QUIET)

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,7 @@
 import './polyfills/EncodedVideoChunk'
+
+import { Log } from 'node-av'
+import { AV_LOG_QUIET } from 'node-av/constants'
+
+// Suppress ffmpeg internal logs (encoder stats, codec info, etc.) during tests
+Log.setLevel(AV_LOG_QUIET)
